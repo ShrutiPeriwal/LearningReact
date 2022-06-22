65d8b14b82ad9e368de31a7521bc6ba8b03b4a98
Shruti <shrutiperiwal21@gmail.com>
2022-06-22 12:42:51 +0530

    meme generator App,js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,22 @@ import Form from "./components/form";
 import "./App.css";
 
 export default function App() {
+ {/*} const [goingOut, setGoingOut] = useState("true");
+  
+  function changeMind() {
+    setGoingOut(prevState => !prevState)
+  } */}
   return (
     <div>
       <Header/>
       <Form />
+     {/* 
+     <div>
+        <h1>Do I feel like going out?</h1>
+        <div onClick = {changeMind}>
+          <h1>{goingOut  ? 'YES' : 'NO'}</h1>
+        </div>
+      </div>  */} 
     </div>
   );
 }
